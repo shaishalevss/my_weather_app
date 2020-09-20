56c91b5f5498ed6b045da27d77f25e1c62e60131
shaishalevss <22shaiss@gmail.com>
2020-09-21 00:00:31 +0300

    added some http get, for using the weather api.

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:my_weather_app/services/location.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -6,17 +11,47 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+
+
+
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
+  }
+
+  void getLocation() async{
+  Location location = Location();
+  await location.getCurrentLocation();
+  print(location.latitude);
+  print(location.longitude);
+}
+
+void getData() async{
+    http.Response response = await http.get('https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=439d4b804bc8187953eb36d2a8c26a02');
+    if(response.statusCode == 200){
+      String data = response.body;
+
+      var decodedData = jsonDecode(data);
+
+      double temp = decodedData['main']['temp'];
+      int condition = decodedData['weather'][0]['id'];
+      String cityName = decodedData['name'];
+
+      print(temp);
+      print(condition);
+      print(cityName);
+
+    } else{
+      print(response.statusCode);
+    }
+}
+
+
   @override
   Widget build(BuildContext context) {
+    getData();
     return Scaffold(
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            //Get the current location
-          },
-          child: Text('Get Location'),
-        ),
-      ),
     );
   }
 }
